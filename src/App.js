e0d422efa34a70d6726fbd5c4d1ef2f500291e73
adityasangani <ays12@iitbbs.ac.in>
2023-10-29 21:05:16 +0530

    checkout and product-detail pages

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { Counter } from './features/counter/Counter';
+import Home from './pages/Home';
+import './App.css';
+import ProductList from './features/product-list/components/ProductList';
+import LoginPage from './pages/LoginPage';
+import Signup from './features/auth/components/Signup';
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import SignupPage from './pages/SignupPage';
+import Cart from './features/cart/Cart';
+import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import ProductDetail from './features/product-list/components/ProductDetail';
+import ProductDetailPage from './pages/ProductDetailPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
+  },
+  {
+    path: "/home",
+    element:<Home></Home>,
+  },
+  {
+    path: "/checkout",
+    element:<Checkout></Checkout>,
+  },
+  {
+    path: "/product-detail",
+    element:<ProductDetailPage></ProductDetailPage>,
+  },
+]);
+
+  
+  function App() {
+    return (
+      <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/features/auth/authAPI.js
@@ -0,0 +1,8 @@
+// export function fetchCount(amount = 1) {
+//   return new Promise((resolve) =>{
+//     const response =  fetch('http://localhost:8080');
+//     const data =  response.json();
+//     resolve({data})
+//   }
+//   );
+// }
