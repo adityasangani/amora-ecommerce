296a5cf7961f90ea6abb4bc6855170ee63c02b80
adityasangani <ays12@iitbbs.ac.in>
2023-11-12 19:13:23 +0530

    filter of category

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,8 +1,23 @@
-// export async function fetchCount(amount = 1) {
-//   return new Promise((resolve) =>{
-//     const response = await fetch('http://localhost:8080');
-//     const data = await response.json();
-//     resolve({data})
-//   }
-//   );
-// }
+export async function fetchAllProducts() {
+  return new Promise(async (resolve) =>{
+    const response = await fetch('http://localhost:8080/products');
+    const data = await response.json();
+    resolve({data})
+  }
+  );
+}
+
+export async function fetchProductsByFilters(filter) {
+  //filter = {"category":"smartphones"}
+  let queryString = '';
+  for(let key in filter){
+    queryString += `${key}=${filter[key]}&`
+  }
+
+  return new Promise(async (resolve) =>{
+    const response = await fetch('http://localhost:8080/products?' + queryString);
+    const data = await response.json();
+    resolve({data})
+  }
+  );
+}
