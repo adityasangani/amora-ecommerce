662d87b210a8a292b43930d33f3ef892f708229c
adityasangani <ays12@iitbbs.ac.in>
2023-11-18 17:28:19 +0530

    pagination and sort fix

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -7,17 +7,32 @@ export async function fetchAllProducts() {
   );
 }
 
-export async function fetchProductsByFilters(filter) {
-  //filter = {"category":"smartphones"}
+export function fetchProductsByFilters(filter, sort, pagination) {
+  //filter = {"category":["smartphones", "laptops"]}
+  //sort = {_sort:"price", _order="desc"}
+  //pagination = {_page:1, _limit = 10} 
   let queryString = '';
   for(let key in filter){
-    queryString += `${key}=${filter[key]}&`
+    const categoryValues = filter[key];
+    if(categoryValues.length){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
+  }
+
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
+  }
+  for(let key in pagination){
+    queryString += `${key}=${pagination[key]}&`
   }
 
   return new Promise(async (resolve) =>{
-    const response = await fetch('http://localhost:8080/products?' + queryString);
+    const response = await fetch('http://localhost:8080/products?' +queryString);
     const data = await response.json();
-    resolve({data})
+    const totalItems = await response.headers.get('X-Total-Count') //provided by json server
+
+    resolve({data:{products:data, totalItems:+totalItems}})
   }
   );
 }
